e15d7b6f412bd753d721707863d4a3c7a820fcca
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2022-02-02 17:25:53 +0100

    Module 00 fix

--- Module_00/ex02/Account.cpp
+++ Module_00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 20:48:38 by fgata-va          #+#    #+#             */
-/*   Updated: 2022/02/01 12:09:56 by fgata-va         ###   ########.fr       */
+/*   Updated: 2022/02/02 17:25:27 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,7 +110,7 @@ bool	Account::makeWithdrawal(int withdrawal) {
 		std::cout << "withdrawal:" << "refused;";
 		successful = false;
 	}
-	std::cout << std::endl;
+	std::cout << "nb_withdrawals:" << this->_nbWithdrawals << std::endl;
 	return (successful);
 }
 
